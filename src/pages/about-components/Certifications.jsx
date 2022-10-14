2eecefebf6e227c17daaed6a54708edf2d2b07bb
Michael <michaeldrpabaya@gmail.com>
2022-10-14 18:39:23 +0800

    About Page Done

--- src/pages/about-components/Certifications.jsx
+++ src/pages/about-components/Certifications.jsx
@@ -1,25 +1,68 @@
 import profile from '../../img/SampleImage.jpg';
+import kodego from '../../img/KodeGoCertificate.PNG';
+import oracle from '../../img/OracleDatabase.PNG';
+import sap from '../../img/SAPBusinessOne.PNG';
+
+import close from '../../img/close.png'
+import { useState } from 'react';
 
 const Certifications = () => {
+
+    const [certifications, setCertifications] = useState(
+        [
+            {id: 1, certificateDescription: 'Sheesh', certificateImg: kodego},
+            {id: 2, certificateDescription: 'Sheesh', certificateImg: oracle},
+            {id: 3, certificateDescription: 'Sheesh', certificateImg: sap},
+        ]
+    );
+
+    const [showImg, setShowImg] = useState(false);
+    const [imgX, setImgX] = useState(null);
+
+    const handleClick = () => {
+        setShowImg(!showImg);
+    };
+
+    const handleImgX = (imgx) => {
+        setImgX(imgx);
+    }
+
     return (
         <>
-            <p className="text-base md:text-4xl font-bold py-5">Certifications</p>
+            {showImg &&
+            <div className='flex justify-center items-center fixed top-0 left-0 z-10 w-full h-full bg-slate-300 dark:bg-gray-800 bg-opacity-50 dark:bg-opacity-40'
+            onClick={handleClick}>
+                <div className='flex justify-center items-center w-full mx-2 md:mx-0 md:w-6/12 md:h-6/12 relative'>
+                    <img src={imgX} alt="Certificate Image" className=''/>
+                </div>
+            </div>
+            }
+
+            <div className='w-10/12 mx-auto  my-5 '>
+                <p className="text-base font-bold text-2xl border-b-solid border-b border-b-black inline">Certifications</p>
+            </div>
 
             <div className='flex flex-col gap-y-2'>
-                <div className='flex flex-col w-10/12 mx-auto'>
-                    <img src={profile} alt="SampleImage" className='rounded' />
-                    <p className='text-base py-1 border-b border-b-black dark:border-b-white mb-2'>Lorem ipsum dolor sit amet.</p>
-                </div>
+                {certifications.map((certificate, key) => {
+                    return(
+                        <div className='flex flex-col w-10/12 mx-auto relative' key={certifications.id}>
+                            
+                            {/* <p className='absolute top-1/3 left-1/3 right-1/3 bottom-1/3 font-semibold text-lg'>From KodeGo</p> */}
+                            
+                            <button onClick={() => {
+                                handleClick();
+                                handleImgX(certificate.certificateImg);
+                            }}>    
+                                <img src={certificate.certificateImg} alt="Certificate Image" className='rounded hover:scale-110 duration-200 border border-solid border-black dark-border-white' />
+                            </button>
 
-                <div className='flex flex-col w-10/12 mx-auto'>
-                    <img src={profile} alt="SampleImage" className='rounded' />
-                    <p className='text-base py-1 border-b border-b-black dark:border-b-white mb-2'>Lorem ipsum dolor sit amet.</p>
-                </div>
-
-                <div className='flex flex-col w-10/12 mx-auto'>
-                    <img src={profile} alt="SampleImage" className='rounded' />
-                    <p className='text-base py-1 border-b border-b-black dark:border-b-white mb-2'>Lorem ipsum dolor sit amet.</p>
-                </div>
+                            <p className='text-start text-base py-2 px-1 border-b border-b-black dark:border-b-white mb-2'>
+                                {/* {certificate.certificateDescription} */}
+                            </p>
+                        
+                        </div>
+                    );
+                })}
             </div>
         </>
     );
--- src/pages/about-components/Experience.jsx
+++ src/pages/about-components/Experience.jsx
@@ -6,42 +6,11 @@ const Experience = () => {
 
     return (
         <>
-            <p className="text-base md:text-4xl font-bold py-5">Experience</p>
+            <div className='w-10/12 mx-auto  my-5 '>
+                <p className="text-base font-bold text-2xl border-b-solid border-b border-b-black inline">Experience</p>
+            </div>
 
             <div className='flex flex-col justify-center items-center'>
-
-                {/* <div className="flex justify-start pr-auto">
-
-                    <div className='w-4/12 bg-gray-500 rounded'>
-                        <p className='p-2'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum, excepturi?</p>
-                    </div>
-
-                    <div className='relative bottom-2.5'>
-                        <div className='border-b border-b-black w-20'>2020</div>
-                    </div>
-
-                    <div >
-                        <img src={Dot} alt="dot" className='h-7 w-7' />
-                    </div>
-
-                    <div className='block border-r border-r-2 border-r-black h-32'></div>
-
-                </div>
-
-                <div className='flex justify-start pl-auto'>
-
-                    <div >
-                        <img src={Dot} alt="dot" className='h-7 w-7' />
-                    </div>
-
-                    <div className='relative bottom-2.5'>
-                        <div className='border-b border-b-black w-20'>2020</div>
-                    </div>
-
-                    <div className='w-4/12 bg-gray-500 rounded'>
-                        <p className='p-2'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum, excepturi?</p>
-                    </div>
-                </div> */}     
 
                 <div className='flex flex-col justify-center w-10/12 relative top-3'>
                     <div className='flex flex-col text-start bg-gray-300 dark:bg-[#272829] rounded drop-shadow-lg p-5 gap-2.5'>
@@ -59,7 +28,7 @@ const Experience = () => {
                 <div className='flex flex-col justify-center w-10/12'>
                     <div className='flex flex-col text-start bg-gray-300 dark:bg-[#272829] rounded drop-shadow-lg p-5 gap-2.5'>
                         <h1 className='text-2xl font-bold'>Student</h1>
-                        <h3 className='text-md font-normal'>STI College Balagtas <span className='text-md font-extralight'>(June-2020 | March-2024)</span></h3>
+                        <h3 className='text-md font-normal'>STI College Balagtas <span className='text-md font-extralight'>(June-2020 | Present)</span></h3>
                         <p className='text-md font-normal'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus autem amet repellendus facilis provident repellat totam ipsa iste? Sint, aspernatur.</p>
                     </div>
 
@@ -70,6 +39,19 @@ const Experience = () => {
                 </div>
                 
                 <div className='flex flex-col justify-center w-10/12 relative bottom-3'>
+                    <div className='flex flex-col text-start bg-gray-300 dark:bg-[#272829] rounded drop-shadow-lg p-5 gap-2.5'>
+                        <h1 className='text-2xl font-bold'>Technician</h1>
+                        <h3 className='text-md font-normal'>Self Employed <span className='text-md font-extralight'>(January-2020 | February-2020)</span></h3>
+                        <p className='text-md font-normal'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus autem amet repellendus facilis provident repellat totam ipsa iste? Sint, aspernatur.</p>
+                    </div>
+
+                    <div className='flex flex-col items-center relative'>
+                        <img src={Dot} alt="dot" className='h-7 w-7 relative bottom-3' />
+                        <div className='border-l border-l-2 border-l-black dark:border-l-white h-14 relative bottom-3'></div>
+                    </div>
+                </div>
+
+                <div className='flex flex-col justify-center w-10/12 relative bottom-6'>
                     <div className='flex flex-col text-start bg-gray-300 dark:bg-[#272829] rounded drop-shadow-lg p-5 gap-2.5'>
                         <h1 className='text-2xl font-bold'>Intern</h1>
                         <h3 className='text-md font-normal'>Amaris PC Laptop Solution <span className='text-md font-extralight'>(September-2019 | December-2019)</span></h3>
@@ -82,7 +64,7 @@ const Experience = () => {
                     </div>
                 </div>
 
-                <div className='flex flex-col justify-center w-10/12 relative bottom-6'>
+                <div className='flex flex-col justify-center w-10/12 relative bottom-9'>
                     <div className='flex flex-col text-start bg-gray-300 dark:bg-[#272829] rounded drop-shadow-lg p-5 gap-2.5'>
                         <h1 className='text-2xl font-bold'>Student</h1>
                         <h3 className='text-md font-normal'>Balagtas National Agricultural High School <span className='text-md font-extralight'>(June-2018 | March-2020)</span></h3>
