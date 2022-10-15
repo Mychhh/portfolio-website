6329306780098b6ef64cd8a8df76c14bac9f7b11
Michael <michaeldrpabaya@gmail.com>
2022-10-15 10:39:11 +0800

    Styling and position on Project created

--- src/pages/project-components/ProjectContent.jsx
+++ src/pages/project-components/ProjectContent.jsx
@@ -7,31 +7,14 @@ const ProjectContent = () => {
         <>
         
             <div className="w-10/12 mx-auto py-5 dark:text-white mt-10">
-                <div className="grid grid-cols-10 gap-10">
+                <div className="grid grid-cols-10 gap-10 justify-center relative">
 
-                    <div className="col-span-10 md:col-span-5">
-                        <img src={Img1} alt="ProjectImage" className='object-contain rounded-lg hover:skew-3 hover:translate-x-6 duration-300'/>
+                    <div className="theproject col-span-10 md:col-span-5 w-full h-80 bg-gradient-to-r from-gray-300 to-gray-700 dark:from-[#272829] dark:to-[#191c1a] relative overflow-hidden">
+                        <p className='absolute top-10 left-10 text-xl font-medium text-white bg-black rounded px-3 z-10'>Name of the project</p>
+                        <button>
+                            <img src={Img1} alt="ProjectImage" className='object-cover h-full w-full hover:scale-150 duration-1000 absolute md:left-1/4 top-0 pr-44 py-5 '/>
+                        </button>
                     </div>
-                    
-                    {/* <div className="col-span-10 md:col-span-5 bg-gray-300">
-                        <img src={Img2} alt="ProjectImage" className='object-contain'/>
-                    </div>
-
-                    <div className="col-span-10 md:col-span-5 bg-gray-300">
-                        <img src={Img1} alt="ProjectImage" className='object-contain'/>
-                    </div>
-                    
-                    <div className="col-span-10 md:col-span-5 bg-gray-300">
-                        <img src={Img2} alt="ProjectImage" className='object-contain'/>
-                    </div>
-
-                    <div className="col-span-10 md:col-span-5 bg-gray-300">
-                        <img src={Img1} alt="ProjectImage" className='object-contain'/>
-                    </div>
-                    
-                    <div className="col-span-10 md:col-span-5 bg-gray-300">
-                        <img src={Img2} alt="ProjectImage" className='object-contain'/>
-                    </div> */}
 
                 </div>
             </div>
